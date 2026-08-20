e163991e1fe02da713a72b0be67f02a5eb2b5f26
Dishank <dishank.sharma.1912@gmail.com>
2026-08-20 16:44:42 +0530

    Expermenting for choosing best model for Target_LTV Regression.

--- src/models/ltv_model.py
+++ src/models/ltv_model.py
@@ -6,13 +6,19 @@ from sklearn.compose import TransformedTargetRegressor
 from src.preprocessing import preprocessor
 
 
-def create_ltv_pipeline(model):
-
-    target_ltv_transformation = TransformedTargetRegressor(regressor=model, func=np.log1p, inverse_func=np.expm1)
+def create_ltv_pipeline(model, transform_target=True):
 
 
-    ltv_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+    if transform_target:
+
+        target_ltv_transformation = TransformedTargetRegressor(regressor=model, func=np.log1p, inverse_func=np.expm1)
+
+        ltv_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
                                ('regressor', target_ltv_transformation)])
+
+    else:
+        ltv_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                                       ('regressor', model)])
 
     return ltv_pipeline
 
